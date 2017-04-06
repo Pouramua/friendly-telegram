7b751cc65431e781694155581ded96cc54b430a9
Pouramua <poukupenga@gmail.com>
2017-04-06 19:08:49 +1200

    Linking and routing all set up for all pages and navbar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 
 
 import Home from './Home'
@@ -11,16 +11,26 @@ import Enrolment from './Enrolment'
 
 const App = () => {
   return (
-    <Router>
-      <div>
-      <Route exact path='/' component={Home} />
-      <Route path='/about' component={About} />
-      <Route path='/teachers' component={Teacher} />
-      <Route path='/events' component={Events} />
-      <Route path='/photos' component={Photos} />
-      <Route path='/enrolment' component={Enrolment} />
-      </div>
-    </Router>
+
+      <Router>
+        <div>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/teachers'>Teacher</Link></li>
+            <li><Link to='/events'>Events</Link></li>
+            <li><Link to='/photos'>Photos</Link></li>
+            <li><Link to='/enrolment'>Enrolment</Link></li>
+          </ul>
+
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/teachers' component={Teacher} />
+          <Route path='/events' component={Events} />
+          <Route path='/photos' component={Photos} />
+          <Route path='/enrolment' component={Enrolment} />
+        </div>
+      </Router>
   )
 }
 
